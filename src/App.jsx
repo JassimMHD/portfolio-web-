7ddb9d124ba4052e3abb02856bc80d19c85b535c
Jassim <jassimj10you@gmail.com>
2025-09-25 20:18:38 +0530

    ract & tailwind css app created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-violet-900">App component</div>
+    </>
+  );
+}
+
+export default App;
